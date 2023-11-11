e52c255f33d1f8b0e756530fa4add57b84509dc3
Sirvau <sirols50984@stud.noroff.no>
2023-11-11 13:33:20 +0100

    Added head, nav and footer to html pages

--- js/api.js
+++ js/api.js
@@ -1,12 +1,12 @@
 
 const urlAllPosts = "https://christmas-blog.siril-vaular.no/wp-json/wp/v2/posts";
 
-
+const corsEnabledUrl = "https://noroffcors.onrender.com/" + urlAllPosts;
 
 async function getPosts() {
-    const response = await fetch(urlAllPosts);
-    console.log(response);
+    const response = await fetch(corsEnabledUrl);
+    const results = await response.json();
+    console.log(results);
 }
-
 
 getPosts();
